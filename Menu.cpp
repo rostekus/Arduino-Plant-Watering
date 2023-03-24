f9ce041bb841ce0a14461c3cb9426f8589f2773e
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-03-24 22:49:33 +0100

    case 'r' tried to do

--- Menu.cpp
+++ Menu.cpp
@@ -1,6 +1,7 @@
   #include "Menu.h"
   #include <string.h>
   #include <LiquidCrystal_I2C.h>
+  //#include <Time.h> //library for displaying the clock
   LiquidCrystal_I2C lcd(0x27,16,2); 
 
 Menu::Menu(char action) {
@@ -15,8 +16,9 @@ void Menu::menu(char c){
   switch (c){
     case 'l':
     if(horizontalIndex==0){
-      lcd.print("Press r");
+      lcd.print("Press r to confirm");
       //digitalClockDisplay();
+      // display main menu
     }
     else if(horizontalIndex==1){
       lcd.print(arr[0]);
@@ -26,6 +28,18 @@ void Menu::menu(char c){
     }
     break;
     case 'r':
+    if (index==0){
+      //Temp sensor function
+    }
+    if (index==1) {
+      //Light sensor function
+    }
+    if (index==2) {
+      //Soil moisture function
+    }
+    else {
+      //Buzzer
+    }
     break;
     case 'u':
     if(index==0) {
