06daf857e6ec64db26b038698390a11c79e29b96
eryk-poradecki <eryk.poradecki@gmail.com>
2023-04-25 22:22:44 +0200

    fix: lowered joystick thresholds

--- src/main/joystick.cpp
+++ src/main/joystick.cpp
@@ -5,35 +5,18 @@ char Joystick::read_input(){
     int x_value = analogRead(VRX_PIN);
     int y_value = analogRead(VRY_PIN);
 
-<<<<<<< HEAD
     if (y_value < 100){
-      //this->last_state = 'u';
         return 'u';
     }
     else if (y_value > 900){
-      //this->last_state = 'd';
         return 'd';
     }
     else if (x_value > 900){
-      //this->last_state = 'r';
-=======
-    if (y_value == 0){
-        return 'd';
-    }
-    else if (y_value == 1023){
-        return 'u';
-    }
-    else if (x_value == 1023){ 
-        return 'l';
-    }
-    else if (x_value == 0 ){
->>>>>>> 6bb9d7ecf702a5992f103791f57da45574b3492d
         return 'r';
     }
     else if (x_value < 100){
-      //this->last_state = 'l';
         return 'l';
     }
 
-    return ' ';
+    return this->last_state;
 }
