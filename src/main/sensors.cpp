0f315c2c155b47d2cf967e9b8b5ebbf5e20afe15
eryk-poradecki <eryk.poradecki@gmail.com>
2023-04-26 19:54:07 +0200

    feat: implemented read method for moisture sensor

--- src/main/sensors.cpp
+++ src/main/sensors.cpp
@@ -1,8 +1,12 @@
 #include "sensors.hpp"
 double MoistureSensor::read() {
-        soilMoistureValue = analogRead(A0);
+        soilMoistureValue = analogRead(A3);
         //soilMoisturePercent = map(soilMoistureValue, airValue, waterValue, 0, 100);
-        return soilMoistureValue;
+        soilMoisturePercent = map(soilMoistureValue, this->airValue, this->waterValue, 0, 100);
+        if(soilMoisturePercent <0){
+          return 0.0;
+        }
+        return soilMoisturePercent;
 }
 
 double LightSensor::read(){
