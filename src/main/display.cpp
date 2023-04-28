cb390e2ca209b1a63bbc9415e71a06e9a19e0051
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-04-28 14:44:25 +0200

    refactor: moved logic out of dispay class

--- src/main/display.cpp
+++ src/main/display.cpp
@@ -1,5 +1,5 @@
 #include "display.hpp"   
-Display::Display(float (*func)(int)) : lcd(0x27, 16, 2), getSensorReading(func) {
+Display::Display() : lcd(0x27, 16, 2){
 }
 
 void Display::init(){
@@ -7,28 +7,14 @@ void Display::init(){
   this -> lcd.backlight();
 }
 
-void Display::printScreen() {
-  float value = this->getSensorReading(this->currentScreen);
+void Display::printDisplay(String firstRow, String secondRow){
   lcd.clear();
-  lcd.print(this->screens[this->currentScreen][0]);
+  lcd.setCursor(0,0);
+  lcd.print(firstRow);
   lcd.setCursor(0,1);
-  if (currentScreen != 3){
-      lcd.print(value);
-  }
-  lcd.print(this->screens[this->currentScreen][1]);
+  lcd.print(secondRow);
 }
 
-void Display::setCurrentScreen(char choice){
-  if (choice == 'd'){
-    if(this->currentScreen<numOfScreens-1)
-    this->currentScreen++;
-    
-        }
-    if (choice == 'u' &&this->currentScreen >0 ){
-    this->currentScreen--;
-        }
-    this->printScreen();
-}
 void Display::print(String str, short row) {
   // value can be 0 or 1
   //lcd.clear();
--- src/main/display.hpp
+++ src/main/display.hpp
@@ -1,22 +1,16 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 #include <LiquidCrystal_I2C.h>
-const int numOfScreens = 4;
 
 class Display{
 
   private:
     LiquidCrystal_I2C lcd;
-    
-    int currentScreen = 0;
-    String screens[numOfScreens][2] = {{"Temperature","C"}, {"Temperature","F"}, 
-    {"Moisture","%"}, {"SD CARD", "Write"} };
-    float (*getSensorReading)(int);
   public: 
-    Display(float (*func)(int));
-    void printScreen();
+    Display();
+  
     void init();
-    void setCurrentScreen(char);
+    void printDisplay(String, String); 
     void print(String, short);
 
 };
